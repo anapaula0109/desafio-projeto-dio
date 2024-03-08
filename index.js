1ec0d70dacb9d9fcf17f0f3491359ad83e8a905a
Ana Paula <apdsmenezes1809@gmail.com>
2024-03-08 03:46:49 -0300

    Último desafio de projedo

---
+++ index.js
@@ -0,0 +1,25 @@
+class Heroi{
+
+    ataque = "";
+
+    constructor(nome, idade, tipo){
+        this.nome = nome;
+        this.idade =  idade;
+        this.tipo = tipo;
+    }
+
+    atacar(){
+
+        if (this.tipo === "mago"){
+            this.ataque = "magia";
+        }else if (this.tipo === "guerreiro"){
+            this.ataque = "espada";
+        }else if (this.tipo === "monge"){
+            this.ataque = "artes marciais";
+        }else if (this.tipo === "ninja"){
+            this.ataque = "shuriken";
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${this.ataque}`)
+    }
+}
